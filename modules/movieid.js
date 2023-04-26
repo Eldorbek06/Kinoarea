445bf73c6dd8e88c87e1483bda71af5a73b17f79
Eldor06 <eldorergashev20sm>
2023-04-26 16:23:23 +0500

    first-section__main html and scss done

--- modules/movieid.js
+++ modules/movieid.js
@@ -1,20 +1,26 @@
 import { headerCreate } from "./ui";
 import { getData } from "./http";
 let header = document.querySelector('header')
-let iframe = document.querySelector('iframe')
+let body = document.body
 
 headerCreate(header)
+
+getData('/movie/popular')
+    .then(res => {
+    })
 
 let movie_id = location.search.split('=').at(-1)
 
 getData(`/movie/${movie_id}`)
-    .then(res => {console.log(res)})
+    .then(res => {
+        console.log(res.data)
+        body.style.backgroundImage = `url(${import.meta.env.VITE_BASE_IMG + res.data.backdrop_path})`
+    })
 
 getData(`/movie/${movie_id}/videos`)
     .then(res => {
         let video = res.data.results[Math.floor(Math.random() * res.data.results.length)]
-        iframe.src = `https://www.youtube.com/embed/${video.key}`;
     })
 
 getData(`/movie/${movie_id}/credits`)
-    .then(res =>console.log({res}))
+    .then(res => console.log({ res }))
