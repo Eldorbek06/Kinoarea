452a1c574b5f4edea58d0f613af0f8baf7e8a8d3
Eldor06 <eldorergashev20sm>
2023-05-03 16:55:32 +0500

    page/main/section:popular-movies completed

--- modules/main.js
+++ modules/main.js
@@ -7,6 +7,7 @@ let first_section_moreBtn = document.querySelector('.first-section .more')
 let trailers__footer = document.querySelector('.trailers__footer')
 let popular_person_cont = document.querySelector('.popular_person_cont')
 let popular_person_cart_cont = document.querySelector('.popular-persons__list')
+let popular_moviesCont = document.querySelector('.popular-movies__slider-container')
 
 headerCreate(header)
 
@@ -47,10 +48,38 @@ getData('/movie/popular')
             }
         })
 
+        reload(res.data.results, popular_moviesCont)
+        let popular_movies_prev = document.querySelector('.popular-movies__slider-prev')
+        let popular_movies_next = document.querySelector('.popular-movies__slider-next')
+        let popular_movies_fraction = document.querySelector('.popular-movies__slider-fraction')
+        let scroll_num = popular_moviesCont.firstElementChild.getBoundingClientRect().width
+        let curr = popular_movies_fraction.firstElementChild
+        let total = popular_movies_fraction.lastElementChild
+        let curr_num = 4
+        let total_num = res.data.results.length++
+        curr.innerHTML = curr_num
+        total.innerHTML = total_num
+
+        popular_movies_next.onclick = () => {
+            if (curr_num !== total_num) {
+                popular_moviesCont.scrollLeft += scroll_num * 4
+                curr_num = curr_num + 4
+                curr.innerHTML = curr_num
+            }
+        }
+        popular_movies_prev.onclick = () => {
+            if (curr_num !== 4) {
+                popular_moviesCont.scrollLeft -= scroll_num * 4
+                curr_num = curr_num - 4
+                curr.innerHTML = curr_num
+            }
+        }
+
+        // console.log();
     })
 
 getData('/person/popular')
-    .then(({data}) => {
-        reloadPopularPerson(data.results.slice(0,2), popular_person_cont)
+    .then(({ data }) => {
+        reloadPopularPerson(data.results.slice(0, 2), popular_person_cont)
         reloadPopularPerson(data.results.slice(2), popular_person_cart_cont)
     })
